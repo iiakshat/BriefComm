135f7eabb7a0e9a9cc93b7f2d0d1a8600b5e13fc
Akshat Sanghvi <jakshat569@gmail.com>
2024-03-26 23:43:03 +0530

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -3,11 +3,5 @@ import os
 
 def translates(text, lang):
     translator = GoogleTranslator(source='english', target=lang)
-
-    # with open('temp/temp.txt', 'w', encoding='utf-8') as f:
-    #     f.write(translator.translate(text))
-    # with open('temp/temp.txt', 'r', encoding='utf-8') as f:
-    #     txt = f.read()
-    # os.remove('temp/temp.txt')
     txt = translator.translate(text)
     return txt
