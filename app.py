022a3d6797e120a12b52a6d56a5bbb333ac099dd
iiakshat <jakshat569@gmail.com>
2024-09-03 22:32:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,6 +99,6 @@ def process_file(filepath, mail, lang, context='some topic', category='transcrip
 
     
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=80)
+    app.run(debug=True, port=80)
 
 
